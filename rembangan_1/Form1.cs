a97ff2109920bb4a9910a8b73823770a1a4d9c8b
AdamDewandaru <adamdewandaru@gmail.com>
2023-06-24 01:59:28 +0700

    Add project files.

---
+++ rembangan_1/Form1.cs
@@ -0,0 +1,35 @@
+namespace rembangan_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void splitContainer1_Panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
